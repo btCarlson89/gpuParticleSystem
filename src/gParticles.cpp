5b45c30130b07eed0407cd546000cdd79152de05
Brian <btCarlson89@gmail.com>
2018-08-04 13:11:34 -0700

    added source files for GParticles class

---
+++ src/gParticles.cpp
@@ -0,0 +1,21 @@
+//
+//  gParticles.cpp
+//  gpuParticleSystem
+//
+//  Created by Brian on 8/4/18.
+//
+//
+
+#include "gParticles.h"
+
+//--------------------------------------------------------------
+GParticles::GParticles()
+{
+    
+}
+
+//--------------------------------------------------------------
+GParticles::~GParticles()
+{
+    
+}
---
+++ src/gParticles.h
@@ -0,0 +1,19 @@
+//
+//  gParticles.h
+//  gpuParticleSystem
+//
+//  Created by Brian on 8/4/18.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+class GParticles
+{
+public:
+    
+    GParticles();
+    ~GParticles();
+};
